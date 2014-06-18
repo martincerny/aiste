f0eddbe270f90a3bfbf9991392e57eaca33d2ae9
lord.domenus <lord.domenus@6de147ab-5aa8-7fd3-14ba-11e5c95acf4f>
2014-06-18 01:10:22 +0000

    dokoncenie controlera + logika prostredia - otestovane

--- trunk/aiste-parent/aiste-simulations/src/main/java/cz/cuni/amis/aiste/simulations/simplefps/Test.java
+++ trunk/aiste-parent/aiste-simulations/src/main/java/cz/cuni/amis/aiste/simulations/simplefps/Test.java
@@ -34,21 +34,40 @@ import org.xml.sax.SAXException;
  */
 public class Test 
 {
-    static int maxSteps = 50;
+    static int maxSteps = 100;
     static int minP = 2;
     static int maxP = 2;
     static String testmap = "/map1.txt";      
     
     public static void main(String args[]) throws FileNotFoundException, ParserConfigurationException, SAXException, IOException
     {
+        System.out.println("Building enviroment ~");
         SimpleFPS fpsEnvironment = new SimpleFPS(minP, maxP, testmap);
+        System.out.println("Enviroment was succesfully created ~");
+        System.out.println("");
+        System.out.println("System strongly recommends to read map before simulating a game.");
+        System.out.println("Press Enter to continue.");
+        System.in.read();
+        
+        System.out.println("Establishing controllers to players ~");
         SimpleFPSReactiveController player1 = new SimpleFPSReactiveController();        
         SimpleFPSReactiveController player2 = new SimpleFPSReactiveController();
+        System.out.println("Controllers succesfully established ~");
+        System.out.println("");
         
+        System.out.println("Setting up enviroment executor ~");
         SynchronuousEnvironmentExecutor executor = new SynchronuousEnvironmentExecutor();
         executor.setEnvironment(fpsEnvironment);
+        System.out.println("Enviroment added ~");
         executor.addAgentController(new AgentExecutionDescriptor(SimpleFPSAgentType.getInstance(), player1, fpsEnvironment));
         executor.addAgentController(new AgentExecutionDescriptor(SimpleFPSAgentType.getInstance(), player2, fpsEnvironment));
+        
+        //player1.onSimulationStep(0);
+        
+        System.out.println("Controllers added ~");
+        System.out.println("Enviroment executor was succesfully created ~");
+        System.out.println("");
+        
         
         IEnvironmentExecutionResult result = executor.executeEnvironment(maxSteps);
         
